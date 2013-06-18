28be86589fe37117dc8ceae62bb7a2dd1d0fdf40
Suraj Tamang <surajyonzon@gmail.com>
2013-06-18 18:05:04 +0800

    added the model for music and video

---
+++ src/main/java/com/swift/example/model/Music.java
@@ -0,0 +1,55 @@
+package com.swift.example.model;
+
+public class Music {
+
+	private String name;
+	
+	private String singer;
+	
+	private String album;
+	
+	private String composer;
+	
+	private String lyricist;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSinger() {
+		return singer;
+	}
+
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getComposer() {
+		return composer;
+	}
+
+	public void setComposer(String composer) {
+		this.composer = composer;
+	}
+
+	public String getLyricist() {
+		return lyricist;
+	}
+
+	public void setLyricist(String lyricist) {
+		this.lyricist = lyricist;
+	}
+	
+}
---
+++ src/main/java/com/swift/example/model/Video.java
@@ -0,0 +1,54 @@
+package com.swift.example.model;
+
+public class Video {
+
+	private String name;
+	
+	private String actor;
+	
+	private String director;
+	
+	private String writer;
+	
+	private String location;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getActor() {
+		return actor;
+	}
+
+	public void setActor(String actor) {
+		this.actor = actor;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
